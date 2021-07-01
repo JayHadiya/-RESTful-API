16c507e3d8704204efff60e6956b986678d893d1
JayHadiya <86783573+JayHadiya@users.noreply.github.com>
2021-07-01 20:18:09 +0530

    Add files via upload

---
+++ app/Http/Controllers/Country/CountryController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Country;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Models\CountryModel;
+
+class CountryController extends Controller
+{
+    public function country(){
+        return response()->json(CountryModel::get(), 200);
+    }
+    public function countryByID($id){
+        $country=CountryModel::find($id);
+        if(is_null($country)){
+            return response()->json('Record not found!', 404);
+        }
+        return response()->json($country, 200);
+}
+public function countrySave(Request $request){
+    $country = CountryModel::create($request->all());
+      return response()->json($country, 201);
+}
+public function countryUpdate(Request $request, $id){
+    $country = CountryModel::find($id);
+    if(is_null($country)){
+        return response()->json('Record not found!', 404);   
+    } 
+    $country->update($request->all());
+      return response()->json($country, 200);
+}
+
+}
